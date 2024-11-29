5e5d078116ec7d6a3a8df31ec891cb99cb2603b9
BSIT3E_CRUZ-JERWIN <202202881@cityofmalabonuniversity.edu.ph>
2024-11-29 12:48:03 +0800

    PORTFOLIO

---
+++ docs/js/script.js
@@ -0,0 +1,7 @@
+function openFAQ() {
+    document.getElementById("faqContainer").style.display = "block";
+}
+
+function closeFAQ() {
+    document.getElementById("faqContainer").style.display = "none";
+}
